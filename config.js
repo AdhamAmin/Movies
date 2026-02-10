0eb672239a9f2e1162950c635e3901083288385a
AdhamAmin <adham2211976@gmail.com>
2026-02-10 20:26:07 +0200

    fix the movies

--- config.js
+++ config.js
@@ -3,5 +3,11 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 const GENRE_LIST_URL = "https://api.themoviedb.org/3/genre/movie/list";
 
+const CONFIG = {
+    BASE_URL: BASE_URL,
+    IMAGE_BASE_URL: IMAGE_BASE_URL,
+    GENRE_LIST_URL: GENRE_LIST_URL
+};
+
 window.CONFIG = CONFIG;
 window.TMDB_API_KEY = TMDB_API_KEY;
